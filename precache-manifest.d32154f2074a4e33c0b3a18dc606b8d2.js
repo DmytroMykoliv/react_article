df98352bd7e4faac370acdbab6c586862b7369cb
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-28 16:32:45 +0200

    Updates

---
+++ precache-manifest.d32154f2074a4e33c0b3a18dc606b8d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ece38585a2cd048da362113021ec50d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a0430284b61550a185",
+    "url": "./static/js/main.c5030c8c.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
